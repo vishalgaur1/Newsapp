a530b60daae3349fb70568761629f183b2748991
Vishal Gaur <vishalgaur2002@gmail.com>
2024-06-26 20:07:07 +0530

    Api Fetch

--- src/components/News.js
+++ src/components/News.js
@@ -2,11 +2,68 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1
+    }
+  }
+
+  async componentDidMount() {
+      this.fetchNews();
+  }
+
+  fetchNews = async () => {
+    this.setState({ loading: true });
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=ea160c6342434d448c1160830c17f304&page=${this.state.page}&pagesize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles, loading: false, totalResults: parsedData.totalResults });
+  };
+
+  handlePrevClick = async () => {
+    this.setState(
+      (prevState) => ({ page: prevState.page - 1 }), 
+      this.fetchNews
+    );
+  };
+
+  handleNextClick = async () => {
+    this.setState(
+      (prevState) => ({ page: prevState.page + 1 }), 
+      this.fetchNews
+    );
+  };
+
   render() {
+
+    const { page, totalResults } = this.state;
+    const pageSize = 20; 
+    const totalPages = Math.ceil(totalResults / pageSize);
+
     return (
-      <div>
-        News
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          {this.state.articles.map((element) => (
+            <div className="col-md-4" key={element.url}>
+              <NewsItem
+                title={element.title ? element.title.slice(0, 57) : ""}
+                description={element.description ? element.description.slice(0, 57) : "Click Read More to know more about this News."}
+                imageUrl={element.urlToImage}
+                newsUrl={element.url}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="container d-flex justify-content-center">
+          <button disabled={this.state.page<=1} type="button" className="btn btn-dark mt-5 mb-5 me-2" onClick={this.handlePrevClick}>&larr; Prev</button>
+          <button disabled={page >= totalPages} type="button" className="btn btn-dark mt-5 mb-5 ms-2" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class NewsItem extends Component {
-  render() {
-    return (
-      <div>
-        NewsItem
-      </div>
-    )
-  }
+    render() {
+        let { title, description, imageUrl, newsUrl } = this.props;
+
+        return (
+            <div className='my-3'>
+                <div className="card" style={{ width: "18rem" }}>
+                    <div style={{
+                        width: "100%",
+                        height: "0",
+                        paddingBottom: "56.25%",
+                        overflow: "hidden",
+                        position: "relative"
+                    }}>
+                        <img src={!imageUrl ?
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWoh8SJC3dyQJ_V1salkOV9HYpJFjeM7Xzcw&s" :
+                            imageUrl}
+                            alt="news"
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                                objectFit: "cover",
+                                position: "absolute"
+                            }}
+                        />
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}.....</p>
+                        <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default NewsItem
+export default NewsItem;
